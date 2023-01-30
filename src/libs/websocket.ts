55d53dc4e02402ea6a5fa1bbf4d704404e2d0e59
Ryan Kirsch <agalloch88@gmail.com>
2023-01-30 15:43:13 -0500

    create send function for websocket, define data object, and set up endpoint

--- src/libs/websocket.ts
+++ src/libs/websocket.ts
@@ -1,16 +1,36 @@
-const websocket = {
-    send: ({
-        data,
-        connectionId,
-    }: {
-        data: {
-            message?: string,
-            type?: string,
-            from?: string,
+import {
+  ApiGatewayManagementApiClient,
+  PostToConnectionCommand,
+  PostToConnectionCommandInput,
+} from "@aws-sdk/client-apigatewaymanagementapi";
 
-        },
-        connectionId: string,
-    }) => {
+export const websocket = {
+  send: ({
+    data,
+    connectionId,
+    domainName,
+    stage
+  }: {
+    data: {
+      message?: string;
+      type?: string;
+      from?: string;
+    };
+    stage: string;
+    domainName: string;
+    connectionId: string;
+  }) => {
+    const client = new ApiGatewayManagementApiClient({
+        endpoint: `https://${domainName}/${stage}`,
+    })
 
+    const params: PostToConnectionCommandInput = {
+        ConnectionId: connectionId,
+        Data: JSON.stringify(data) as any,
     }
-}
+
+    const command = new PostToConnectionCommand(params);
+
+    return client.send(command);
+  },
+};
