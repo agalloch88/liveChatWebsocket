77ce03ec30adcd8665a7dccf4ac37101185a009d
Ryan Kirsch <agalloch88@gmail.com>
2023-02-04 16:15:19 -0500

    copy skeleton from message, rip out most functionality to prep for disconnect

---
+++ src/functions/disconnect/index.ts
@@ -0,0 +1,24 @@
+import { formatJSONResponse } from "@libs/apiGateway";
+import { APIGatewayProxyEvent } from "aws-lambda";
+import { dynamo } from "@libs/dynamo";
+import { UserConnectionRecord } from "src/types/dynamo";
+import { websocket } from "@libs/websocket";
+
+export const handler = async (event: APIGatewayProxyEvent) => {
+  try {
+    const tableName = process.env.roomConnectionTable;
+    // destructuring the requestContext object
+    const { connectionId} = event.requestContext;
+    
+
+    return formatJSONResponse({});
+  } catch (error) {
+    console.log(error);
+    return formatJSONResponse({
+      statusCode: 502,
+      data: {
+        message: error.message,
+      },
+    });
+  }
+};
